fdcdb50bfa498533c2a1e9ba73c530f55edbee96
Jacob Marshall <git@jacobmarshall.co>
2017-04-18 22:47:59 +1200

    Add menubar icon template

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const menubar = require('menubar');
 
 const mb = menubar({
   index: 'http://localhost:3000',
+  icon: __dirname + '/src/assets/menubar/menubar_iconTemplate.png',
   preloadWindow: true,
   width: 380,
   height: 42,
